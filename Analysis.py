43b6d6c3a078325631a02b538a133ec46bdac1b9
Bhanuuteja <103250285+Bhanuuteja@users.noreply.github.com>
2024-02-05 13:59:44 +0530

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -110,7 +110,7 @@ st.pyplot(plt)
 st.write("Model Comparisons:")
 st.write(f"Decision Tree Accuracy: {rf_accuracy * 100:.2f}%")
 st.write(f"Logistic Regression Accuracy: {logistic_accuracy * 100:.2f}%")
-#st.write(f"K-Means Accuracy: {kmeans_accuracy *100:.2f}%")
+st.write(f"K-Means Accuracy: {kmeans_accuracy *100:.2f}%")
 
 model_names = ["Decision Tree", "Logistic Regression"]
 accuracies = [rf_accuracy, logistic_accuracy]
